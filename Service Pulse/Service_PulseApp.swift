7b4de4c78543b3de52facecef531e0dc72a0671d
Kobi Bell <bellkobis@gmail.com>
2026-06-11 17:30:13 -0700

    Initial Commit

---
+++ Service Pulse/Service_PulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Service_PulseApp.swift
+//  Service Pulse
+//
+//  Created by Kobi Bell on 6/11/26.
+//
+
+import SwiftUI
+
+@main
+struct Service_PulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
